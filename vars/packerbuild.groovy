a1b1be3aa9a7e5b634a4cf0a02b702232bc49b4d
polaristechverse <polaristechverse@gmail.com>
2026-04-17 13:54:42 +0530

    Update packerbuild.groovy

--- vars/packerbuild.groovy
+++ vars/packerbuild.groovy
@@ -2,4 +2,5 @@ def call() {
     echo "Building ami with packer..."
     sh  'packer plugins install github.com/hashicorp/amazon'
     sh 'packer validate --var-file packer-vars.json packer.json'
+    sh 'packer build --var-file packer-vars.json packer.json '
 }
